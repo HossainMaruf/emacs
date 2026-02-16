fa53db3a8a2b30e78650c08f56164978d08f5718
HossainMaruf <maruficepustian@gmail.com>
2026-02-16 16:32:37 +0600

    Vim like jk or kj added to exit insert mode

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 (global-display-line-numbers-mode 1)
 (electric-pair-mode 1)
 
-(set-face-attribute 'default nil :font "Monaco" :height 140 :weight 'regular)
+(set-face-attribute 'default nil :font "Monaco" :height 130 :weight 'regular)
 ;; (load-theme 'tango-dark t)
 
 (winner-mode 1)
@@ -41,8 +41,6 @@
   :config (which-key-mode))
 
 (use-package dired)
-;; Dired Improvements
-(setq dired-listing-switches "-alh")
 
 (add-hook 'prog-mode-hook 'flymake-mode)
 
@@ -76,6 +74,14 @@
   ;; (setq evil-collection-mode-list '(calendar dashboard dired ediff info magit ibuffer))
   (add-to-list 'evil-collection-mode-list 'help) ;; evilify help mode
   (evil-collection-init))
+
+(use-package key-chord
+  :ensure t
+  :config
+  (key-chord-mode 1)
+  (setq key-chord-two-keys-delay 0.2)
+  (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+  (key-chord-define evil-insert-state-map "kj" 'evil-normal-state))
 
 (use-package company
   :defer 2
@@ -412,12 +418,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons-dired all-the-icons-ivy-rich company counsel dashboard
-			 doom-modeline evil evil-collection general
-			 lsp-java org-bullets projectile)))
+   '(ace-window all-the-icons-dired all-the-icons-ivy-rich counsel
+		dashboard doom-modeline evil evil-collection general
+		key-chord lsp-java org-bullets projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+(put 'dired-find-alternate-file 'disabled nil)
